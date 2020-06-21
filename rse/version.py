0b6c4ece41547097de1e44c77f9de3839d78303d
vsoch <vsochat@stanford.edu>
2020-06-21 14:12:49 -0600

    need html5lib for scraping
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- rse/version.py
+++ rse/version.py
@@ -30,7 +30,10 @@ APP_REQUIRES = (
     ("gevent-websocket", {"min_version": "0.10.1"}),
 )
 
-SCRAPER_REQUIRES = (("beautifulsoup4", {"min_version": "4.9.0"}),)
+SCRAPER_REQUIRES = (
+    ("beautifulsoup4", {"min_version": "4.9.0"}),
+    ("html5lib", {"min_version": "1.0.1"}),
+)
 DATABASE_REQUIRES = (("sqlalchemy", {"min_version": None}),)
 TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
 
